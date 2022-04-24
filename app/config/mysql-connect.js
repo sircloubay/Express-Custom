41e9e015648b58db5f6b74e9d40639de6f0fa089
sircloubay <bayu.firmansyah@mail.sirclo.com>
2022-04-24 17:54:19 +0700

    add a file config mysql

---
+++ app/config/mysql-connect.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql')
+
+// configuration in database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'express'
+})
+
+//create connection
+connection.connect((err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Database Connected')
+  }
+})
+
+module.exports = connection
