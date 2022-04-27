9d8e66c36e39dc089d687e576778d81c87538098
sircloubay <bayu.firmansyah@mail.sirclo.com>
2022-04-28 01:17:36 +0700

    update miror file

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,4 +1,4 @@
-// const session = require('express-session')
+const session = require('express-session')
 const home = require('../controller/home')
 
 module.exports = (app) => {
@@ -11,39 +11,39 @@ module.exports = (app) => {
       })
   })
 
-  // app.get('/data/:table', (req, res) => {
-  //   home.getDataCollection(req, res)
-  // })
+  app.get('/data/:table', (req, res) => {
+    home.getDataCollection(req, res)
+  })
 
-  // app.get('/data/:table/:field/:value', (req, res) => {
-  //   home.getDataDetail(req, res)
-  // })
+  app.get('/data/:table/:field/:value', (req, res) => {
+    home.getDataDetail(req, res)
+  })
 
-  // app.put('/', (req, res) => {
-  //   home.update(req, res)
-  // })
+  app.put('/', (req, res) => {
+    home.update(req, res)
+  })
 
-  // app.post('/', (req, res) => {
-  //   home.insert(req, res)
-  // })
+  app.post('/', (req, res) => {
+    home.insert(req, res)
+  })
 
-  // app.delete('/', (req, res) => {
-  //   home.deletes(req, res)
-  // })
+  app.delete('/', (req, res) => {
+    home.deletes(req, res)
+  })
 
-  // app.get('/auth',(req, res) => {
-  //   req.session.userId = "mansbjdhsfa8765"
-  //   req.session.save()
-  //   res.redirect('/')
-  // })
+  app.get('/auth',(req, res) => {
+    req.session.userId = "mansbjdhsfa8765"
+    req.session.save()
+    res.redirect('/')
+  })
 
-  // app.get('/logout',(req, res) => {
-  //   req.session.destroy()
-  //   res.end('anda telah logout')
-  // })
+  app.get('/logout',(req, res) => {
+    req.session.destroy()
+    res.end('anda telah logout')
+  })
 
-  // app.post('/sql',(req, res) => {
-  //   home.querySQL(req.body.sql, res)
-  // })
+  app.post('/sql',(req, res) => {
+    home.querySQL(req.body.sql, res)
+  })
 
 }
